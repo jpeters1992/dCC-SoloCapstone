9610cefc5f42f043fe383f3b2415e4636897c52f
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2019-11-25 13:48:01 -0600

    init commit

---
+++ dCC-SoloCapstone/Awarness/Awarness/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Awarness.Startup))]
+namespace Awarness
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
