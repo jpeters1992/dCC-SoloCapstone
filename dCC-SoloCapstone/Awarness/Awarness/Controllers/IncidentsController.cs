8390a4d659542dce0cb6d720230527d49bd1998d
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2020-01-10 15:07:36 -0600

    added the incident controller logic and changed the title of application

--- dCC-SoloCapstone/Awarness/Awarness/Controllers/IncidentsController.cs
+++ dCC-SoloCapstone/Awarness/Awarness/Controllers/IncidentsController.cs
@@ -19,29 +19,31 @@ namespace Awarness.Controllers
         // GET: Incidents
         public ActionResult Index()
         {
-            return View();
+            return View(context.Incidents.ToList());
         }
 
         // GET: Incidents/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Incident incident = context.Incidents.Where(i => i.IncidentReferenceNumber == id).SingleOrDefault();
+            return View(incident);
         }
 
         // GET: Incidents/Create
         public ActionResult Create()
         {
-            return View();
+            Incident newIncident = new Incident();
+            return View(newIncident);
         }
 
         // POST: Incidents/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Incident incident)
         {
             try
             {
-                // TODO: Add insert logic here
-
+                context.Incidents.Add(incident);
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -53,16 +55,23 @@ namespace Awarness.Controllers
         // GET: Incidents/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Incident incident = context.Incidents.Where(i => i.IncidentReferenceNumber == id).SingleOrDefault();
+            return View(incident);
         }
 
         // POST: Incidents/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Incident incident)
         {
             try
             {
-                // TODO: Add update logic here
+                Incident editedIncident = context.Incidents.Find(id);
+                editedIncident.IncidentType = incident.IncidentType;
+                editedIncident.IncidentTime = incident.IncidentTime;
+                editedIncident.IncidentDate = incident.IncidentDate;
+                editedIncident.IncidentLocation = incident.IncidentLocation;
+                editedIncident.AdditionalDetails = incident.AdditionalDetails;
+                context.SaveChanges();
 
                 return RedirectToAction("Index");
             }
@@ -75,17 +84,18 @@ namespace Awarness.Controllers
         // GET: Incidents/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Incident incident = context.Incidents.Where(i => i.IncidentReferenceNumber == id).SingleOrDefault();
+            return View(incident);
         }
 
         // POST: Incidents/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id, Incident incident)
         {
             try
             {
-                // TODO: Add delete logic here
-
+                context.Incidents.Remove(context.Incidents.Find(id));
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
